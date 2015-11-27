2107b4c591f66b6ee4e0168e2c6497e436928c8e
Thomas Solow <thomassolow@gmail.com>
2015-11-27 15:17:13 -0500

    mostly done, need to hide/show select menu and add blank field in select menu

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -7,8 +7,7 @@ $("#search").on("submit", function(e){
 })
 
 function getAPI(keyword){
-  url = "http://www.omdbapi.com/?s="+keyword.replace(/ /g,'+')+"&y=&plot=short&r=json"
-
+  url = "http://www.omdbapi.com/?s="+keyword.replace(/ /g,'+')+"&y=&plot=short&r=json";
   $.ajax({
     url: url,
     type: "get",
@@ -16,8 +15,28 @@ function getAPI(keyword){
   }).done(function(response){
     $("#movie-select").empty() //empty the select field before populating it
     for (var i=0; i<response.Search.length; i++){
-      $("#movie-select").append($('<option>'+response.Search[i].Title+'</option>'))
+      $("#movie-select").append($('<option id='+response.Search[i].imdbID+'>'+response.Search[i].Title+'</option>'))
     }
+    addListener();
   })
-
+}
+function addListener(){
+  //check for a change in the select menu
+  $("#movie-select").change("submit", function(e){
+    //make a new ajax call based on the id given to the selected
+    //element
+    $.ajax({
+      url:"http://www.omdbapi.com/?i="+$('select option:selected' ).attr('id')+"&y=&plot=short&r=json",
+      type:"get",
+      dataType: "json"
+    }).done(function(response){
+      //empty info div, then append information from the json
+      $("#movie-detail").empty()
+      $("#movie-detail").append($("<img src="+response.Poster+"/>"))
+      $("#movie-detail").append($("<p>Title: "+response.Title+"</p>"+
+      "<p>Director: "+response.Director+"</p>"+
+      "<p>Actors: "+response.Actors+"</p>"+
+      "<p>Country: "+response.Country+"</p>"))
+    })
+  })
 }
