15b2730800a701ee4598f98a34741b447c5b64c4
Thomas Solow <thomassolow@gmail.com>
2015-11-27 14:10:22 -0500

    populating select menu done

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,2 +1,23 @@
-// API Docs at: 
+// API Docs at:
 // http://www.omdbapi.com
+$("#search").on("submit", function(e){
+
+  e.preventDefault();
+  getAPI($("#movie-search").val());
+})
+
+function getAPI(keyword){
+  url = "http://www.omdbapi.com/?s="+keyword.replace(/ /g,'+')+"&y=&plot=short&r=json"
+
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done(function(response){
+    $("#movie-select").empty() //empty the select field before populating it
+    for (var i=0; i<response.Search.length; i++){
+      $("#movie-select").append($('<option>'+response.Search[i].Title+'</option>'))
+    }
+  })
+
+}
