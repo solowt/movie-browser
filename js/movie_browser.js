b455231c02419695f727d1ff1557042d7abd1246
Thomas Solow <thomassolow@gmail.com>
2015-11-27 16:22:14 -0500

    all done i think

--- js/movie_browser.js
+++ js/movie_browser.js
@@ -1,7 +1,8 @@
-$("#search").on("submit", function(e){
-
-  e.preventDefault();
-  getAPI($("#movie-search").val());
+$("document").ready(function(){
+  $("#search").on("submit", function(e){
+    e.preventDefault();
+    getAPI($("#movie-search").val());
+  })
 })
 
 function getAPI(keyword){
@@ -11,32 +12,36 @@ function getAPI(keyword){
     type: "get",
     dataType: "json"
   }).done(function(response){
-    $("#movie-select").children().slice(1).remove() //empty the select field before populating it
-    //$("#movie-select").children()[0].css("Value")="Select a movie ("+keyword+")"
+    $("#movie-select").children().remove() //empty the select field before populating it
+    //define the first option
+    $("<option value=''>Results for: "+keyword+"</option>").appendTo($("#movie-select"))
     for (var i=0; i<response.Search.length; i++){
       $("#movie-select").append($('<option id='+response.Search[i].imdbID+'>'+response.Search[i].Title+'</option>'))
     }
     addListener();
+    //show the dropdown menu, it's hidden by default
     $("#movie-select").show();
   })
 }
 function addListener(){
   //check for a change in the select menu
   $("#movie-select").change("submit", function(e){
-    //make a new ajax call based on the id given to the selected
-    //element
-    $.ajax({
-      url:"http://www.omdbapi.com/?i="+$('select option:selected' ).attr('id')+"&y=&plot=short&r=json",
-      type:"get",
-      dataType: "json"
-    }).done(function(response){
-      //empty info div, then append information from the json
-      $("#movie-detail").empty()
-      $("#movie-detail").append($("<img src="+response.Poster+"/>"))
-      $("#movie-detail").append($("<p>Title: "+response.Title+"</p>"+
-      "<p>Director: "+response.Director+"</p>"+
-      "<p>Actors: "+response.Actors+"</p>"+
-      "<p>Country: "+response.Country+"</p>"))
-    })
+    if ($('select option:selected').attr('id')) {
+      //make a new ajax call based on the id given to the selected
+      //element
+      $.ajax({
+        url:"http://www.omdbapi.com/?i="+$('select option:selected'  ).attr('id')+"&y=&plot=short&r=json",
+        type:"get",
+        dataType: "json"
+      }).done(function(response){
+        //empty info div, then append information from the json
+        $("#movie-detail").empty()
+        $("#movie-detail").append($("<img src="+response.Poster+"/>"))
+        $("#movie-detail").append($("<p>Title: "+response.Title+"</p>"+
+        "<p>Director: "+response.Director+"</p>"+
+        "<p>Actors: "+response.Actors+"</p>"+
+        "<p>Country: "+response.Country+"</p>"))
+      })
+    }
   })
 }
